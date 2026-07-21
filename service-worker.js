57c7c45d332863a97ac1fa8f48f191cb20324e29
chitranshcolorlab <sharadmn29@gmail.com>
2026-07-21 10:41:58 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='chitransh-v2';
+const CACHE='chitransh-v3-premium';
 const ASSETS=["./", "./index.html", "./style.css", "./script.js", "./manifest.json", "./contact.vcf", "./logo.png", "./hero.webp", "./icon-192.png", "./icon-512.png", "./photo-01.webp", "./photo-02.webp", "./photo-03.webp", "./photo-04.webp", "./photo-05.webp", "./photo-06.webp", "./photo-07.webp", "./photo-08.webp", "./photo-09.webp", "./photo-10.webp", "./photo-11.webp", "./photo-12.webp", "./photo-13.webp", "./photo-14.webp", "./photo-15.webp", "./photo-16.webp", "./photo-17.webp", "./photo-18.webp", "./photo-19.webp", "./photo-20.webp"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
